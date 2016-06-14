c898cbac4d0fcc15e186b6ce2e1203a232178ba8
evgeny <sandmangood@gmail.com>
2016-06-14 10:53:50 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,104 @@
+$(window).load(function(){
+
+	// about parallax
+
+	$(window).stellar({
+		horizontalOffset: 0
+	});
+
+	
+
+    
+
+
+
+
+
+
+
+
+
+});
+
+
+
+$(function(){
+
+	// about parallax
+
+	$('.about-content_box > img').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn', 
+		offset: 100 
+    });
+
+
+    // dates-slider
+
+    $('.js-dates-slider').bxSlider({
+	  mode: 'fade',
+	  pager: true,
+	  controls: false,
+	  auto: true,
+	  pause: 3000
+	});
+
+	// users slider
+
+	$('.js-users-list').owlCarousel({
+	    margin: 29,
+	    items: 6,
+	    mouseDrag: false
+	})
+
+
+	 
+   
+
+
+
+
+
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
