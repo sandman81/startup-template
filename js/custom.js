5f7591f6c436d9877cc69fc6fa714b473529e999
sandman81 <sandmangood@gmail.com>
2016-06-29 14:31:20 +0300

    add video modals

--- js/custom.js
+++ js/custom.js
@@ -122,6 +122,133 @@ $(function(){
       $(this).addClass('active').siblings().removeClass('active').closest('.modal-popup').find('.modal-popup-content-users').eq($(this).index()).fadeIn(200).siblings('.modal-popup-content-users').hide();
     });
 
+    // modal video
+
+    $(".js-video-button").click(function(){
+    	$('.js-video-modal').addClass('active');
+        var source = $(this).attr('video-source');
+        $('.js-video-modal iframe').attr('src', source);
+    });
+
+    $('.js-video-modal-closed').click(function(event) {
+    	$('.js-video-modal').removeClass('active');
+    	$('.js-video-modal iframe').attr('src', '');
+    });
+
+    // scroll elements
+
+    $('.js-top-button-1').bind('click', function (e) {
+       var scroll = $(".about-content").offset().top - 92;
+       $('body,html').animate({
+            scrollTop: scroll
+        }, 500);
+        return false;  
+    });
+
+    $('.js-top-button-2').bind('click', function (e) {
+       var scroll = $(".reviews-section").offset().top - 92;
+       $('body,html').animate({
+            scrollTop: scroll
+        }, 500);
+        return false;  
+    });
+
+    $('.js-top-button-3').bind('click', function (e) {
+       var scroll = $(".graduates-section").offset().top - 92;
+       $('body,html').animate({
+            scrollTop: scroll
+        }, 500);
+        return false;  
+    });
+
+    // $('.js-top-button-4').bind('click', function (e) {
+    //    var scroll = $(".graduates-section").offset().top - 92;
+    //    $('body,html').animate({
+    //         scrollTop: scroll
+    //     }, 500);
+    //     return false;  
+    // });
+
+    $('.js-register-button').bind('click', function (e) {
+       var scroll = $(".register-section").offset().top - 92;
+       $('body,html').animate({
+            scrollTop: scroll
+        }, 500);
+        return false;  
+    });
+
+    $('.js-info-button-1').bind('click', function (e) {
+       var scroll = $(".users-info").offset().top - 92;
+       $('body,html').animate({
+            scrollTop: scroll
+        }, 500);
+        return false;  
+    });
+
+    $('.js-info-button-2').bind('click', function (e) {
+       var scroll = $(".programm-content").offset().top - 92;
+       $('body,html').animate({
+            scrollTop: scroll
+        }, 500);
+        return false;  
+    });
+
+    $('.js-info-button-3').bind('click', function (e) {
+       var scroll = $(".place-content").offset().top - 92;
+       $('body,html').animate({
+            scrollTop: scroll
+        }, 500);
+        return false;  
+    });
+
+    $('.js-info-button-4').bind('click', function (e) {
+       var scroll = $(".price-section").offset().top - 92;
+       $('body,html').animate({
+            scrollTop: scroll
+        }, 500);
+        return false;  
+    });
+
+    $('.js-gallery-button').bind('click', function (e) {
+       var scroll = $(".gallery-section").offset().top - 92;
+       $('body,html').animate({
+            scrollTop: scroll
+        }, 500);
+        return false;  
+    });
+
+
+    // modals open
+
+    $('.js-all-speakers-button').click(function(event) {
+    	$('.js-modal-speakers').addClass('active');
+    });
+
+    $('.js-all-mentors-button').click(function(event) {
+    	$('.js-modal-mentors').addClass('active');
+    });
+
+    $('.js-all-reviews-button').click(function(event) {
+    	$('.js-modal-reviews').addClass('active');
+    });
+
+    $('.js-all-graduates-button').click(function(event) {
+    	$('.js-modal-graduates').addClass('active');
+    });
+
+
+
+
+    $('.modal-popup-top_closed-button').click(function(event) {
+    	$(this).closest('.modal-content').removeClass('active');
+    });
+
+
+
+
+
+
+
 
     
 
